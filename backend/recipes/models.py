912481fde10140b28bd5b0081102dc83099b18c7
Алексей <aaamegulya@gmail.com>
2023-02-28 15:42:09 +0500

    add models to recipes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,147 @@
+from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    '''Model for recipes tags.'''
+    name = models.CharField(
+        'Название',
+        max_length=200
+    )
+    color = models.CharField(
+        'Цвет в HEX',
+        max_length=7,
+        null=True,
+        validators=[
+            RegexValidator(
+                '^#([a-fA-F0-9]{6})',
+                message='Введите HEX-код цвета.'
+            )
+        ]
+
+    )
+    slug = models.SlugField(
+        'Слаг',
+        max_length=200,
+        unique=True,
+        null=True
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=200
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
+
+
+class Recipe(models.Model):
+    '''Model of recipe.'''
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+
+    image = models.ImageField(
+        upload_to='recipes/',
+        verbose_name='Картинка'
+    )
+
+    text = models.TextField(
+        verbose_name='Описание'
+    )
+
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientRecipe',
+        verbose_name='Ингредиенты',
+        related_name='recipes',
+        blank=False
+    )
+
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        related_name='recipes',
+    )
+
+    cooking_time = models.IntegerField(
+        validators=[
+            MinValueValidator(1, 'Время приготовления не менее минуты.'),
+        ],
+        verbose_name='Время приготовления в минутах'
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    class Meta:
+        ordering = ['-created']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'name'],
+                name='unique_author_name')
+        ]
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class Favorite(models.Model):
+    '''Model of favorite recipes.'''
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorite_user',
+        verbose_name='Подписчик'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorite_recipe',
+        verbose_name='Рецепт'
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_favorite'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user.username} - {self.recipe.name}'
